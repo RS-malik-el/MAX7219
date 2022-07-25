debe4b784631a71370ebe719556e4c6c8fc52abe
RS_Exau_Kim <92597014+RS-malik-el@users.noreply.github.com>
2022-07-26 01:36:20 +0200

    Add files via upload

---
+++ MATRICE_LED_8x8_MAX7219CNG/MATRICE_LED_8x8_MAX7219CNG.ino
@@ -0,0 +1,54 @@
+/**
+ * e-mail : rachelsysteme@gmail.com
+ * gitHub : https://github.com/RS-malik-el
+ * Donation : paypal.me/RachelSysteme
+ * 
+ * @AUTHEUR : RACHEL SYSTEME
+ * DATE : 25/07/2022
+ * 
+ * @Board : arduino uno
+ * 
+ * matrice leds 8x8 max7219
+ * 
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+**/
+
+#include <simple_matrix.h>
+
+#define DEL 250 // Temps de défilement des caractères
+#define CS_PIN 2 
+
+simpleMatrix afficheur(CS_PIN,false,2); 
+
+const char msg1[] PROGMEM = "MERCI DE VOTRE CONFIANCE";
+char msg[] = "MERCI DE VOTRE CONFIANCE";
+char msg2[] = "ABONNEZ VOUS A NOTRE CHAINE YOUTUBE : RACHEL SYSTEME";
+
+void setup() {
+  afficheur.begin();
+  afficheur.setIntensity(3);
+  afficheur.clearDisplay();
+}
+
+void loop() {
+  //afficheur.scrollTextPROGMEMRightToLeft(msg1,DEL);
+  //afficheur.scrollTextPROGMEMLeftToRight(msg1,DEL);
+
+  //afficheur.scrollText(msg,DEL,false);
+  //afficheur.scrollText(msg,DEL,true);
+
+  afficheur.scrollTextRightToLeft(msg,DEL);
+  afficheur.scrollTextLeftToRight(msg,DEL);
+  delay(500);
+  afficheur.print(msg2,0,DEL,false);
+  delay(500);
+}
---
+++ MATRICE_LED_8x8_MAX7219CNG_CHAR/MATRICE_LED_8x8_MAX7219CNG_CHAR.ino
@@ -0,0 +1,73 @@
+/**
+ * e-mail : rachelsysteme@gmail.com
+ * gitHub : https://github.com/RS-malik-el
+ * Donation : paypal.me/RachelSysteme
+ * 
+ * @AUTHEUR : RACHEL SYSTEME
+ * DATE : 25/07/2022
+ * 
+ * @Board : arduino uno
+ * 
+ * matrice leds 8x8 max7219
+ * 
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+**/
+
+#include <simple_matrix.h>
+
+#define DEL 250 // Temps de défilement des caractères
+#define CS_PIN 2 
+
+simpleMatrix afficheur(CS_PIN,false,2); 
+
+uint8_t coeur[8]={
+  B01100000,
+  B11110000,
+  B11111000,
+  B01111100,
+  B01111110,
+  B11111100,
+  B11110000,
+  B01100000
+};
+
+uint8_t c[16]={// pouce et coeur 
+  B00000000,
+  B00111110,
+  B00111110,
+  B00111110,
+  B11111110,
+  B11111110,
+  B00011110,
+  B00000000,
+  B01100000,
+  B11110000,
+  B11111000,
+  B01111100,
+  B01111110,
+  B11111100,
+  B11110000,
+  B01100000
+};
+
+void setup() {
+  afficheur.begin();
+  afficheur.setIntensity(3);
+  afficheur.clearDisplay();
+}
+
+void loop() {
+  afficheur.scrollBuffer(coeur,DEL,8);
+  delay(500);
+  afficheur.scrollBuffer(c,DEL,16);
+  delay(500);
+}
